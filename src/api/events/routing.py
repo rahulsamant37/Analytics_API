a202aa1748609b60f7e7906dc6f6849367379862
Rahul Samant <161972011+rahulsamant37@users.noreply.github.com>
2025-06-10 23:48:26 +0000

    feat: Creating Endpoint to retrieve a specific event by its ID with data validation using pydantic

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from .schema import EventSchema
 
 router = APIRouter()
 
@@ -8,5 +9,12 @@ def read_events():
     Endpoint to retrieve a list of events.
     """
     return {
-        "items": [1,2,3]
+        "results": [1,2,3]
     }
+
+@router.get("/{event_id}")
+def get_event(event_id: int)-> EventSchema:
+    """
+    Endpoint to retrieve a specific event by its ID.
+    """
+    return {"id": event_id}
